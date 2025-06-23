9a8bf76ed215a67d8aeffa4174dd55f6cca0ef1c
zoom1954 <zizomzoom@gmail.com>
2025-06-23 13:40:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-
+body {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  background-color: #f0f2f5;
+}
+h1 {
+  color: #1877f2;
+}
+input {
+  padding: 10px;
+  width: 300px;
+  margin: 10px;
+}
+button {
+  padding: 10px 20px;
+  background-color: #1877f2;
+  color: white;
+  border: none;
+  cursor: pointer;
+  margin: 5px;
+}
+button:hover {
+  background-color: #165db7;
+}
+ul {
+  list-style: none;
+  padding: 0;
+}
+li {
+  margin: 5px 0;
+}
